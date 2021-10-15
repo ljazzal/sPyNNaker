337e06364b0b9596aff17b3cc96a38dffb6f3d08
ljazzal <jazzalin.main@gmail.com>
2021-10-15 13:21:44 -0400

    Cleanup

--- python_models8/neuron/plasticity/stdp/timing_dependence/tm_timing_dependence.py
+++ python_models8/neuron/plasticity/stdp/timing_dependence/tm_timing_dependence.py
@@ -32,7 +32,7 @@ class TsodyksMarkramTimingDependence(AbstractTimingDependence):
             # TODO: update parameters
             tau_f,
             tau_d,
-            tau_syn,
+            tau_syn=1.0,
 
             A_plus=1.0, A_minus=1.0):
 
@@ -52,7 +52,7 @@ class TsodyksMarkramTimingDependence(AbstractTimingDependence):
         ts = machine_time_step_ms()
         self._tau_f_data = get_exp_lut_array(ts, self._tau_f)
         self._tau_d_data = get_exp_lut_array(ts, self._tau_d)
-        self._tau_syn_data = get_exp_lut_array(ts, self._tau_syn)
+        # self._tau_syn_data = get_exp_lut_array(ts, self._tau_syn)
 
     # TODO: Add getters and setters for parameters
 
@@ -88,8 +88,8 @@ class TsodyksMarkramTimingDependence(AbstractTimingDependence):
 
         # TODO: update to check parameters are equal
         return (self._tau_f == timing_dependence.tau_f and
-                self._tau_d == timing_dependence.tau_d and 
-                self._tau_syn == timing_dependence.tau_syn)
+                self._tau_d == timing_dependence.tau_d)# and 
+                # self._tau_syn == timing_dependence.tau_syn)
 
     @property
     def vertex_executable_suffix(self):
@@ -112,9 +112,9 @@ class TsodyksMarkramTimingDependence(AbstractTimingDependence):
     @overrides(AbstractTimingDependence.get_parameters_sdram_usage_in_bytes)
     def get_parameters_sdram_usage_in_bytes(self):
         # TODO: update to match the number of bytes used by the parameters
-        return BYTES_PER_WORD * (len(self._tau_f_data) +
-                                 len(self._tau_d_data) +
-                                 len(self._tau_syn_data))
+        return (2 * BYTES_PER_WORD) * (len(self._tau_f_data) +
+                                 len(self._tau_d_data))# +
+                                #  len(self._tau_syn_data))
 
     @property
     def n_weight_terms(self):
@@ -130,20 +130,20 @@ class TsodyksMarkramTimingDependence(AbstractTimingDependence):
         # Write parameters (fixed point) to spec
         fixed_point_tau_f = float_to_fixed(self._tau_f)
         fixed_point_tau_d = float_to_fixed(self._tau_d)
-        fixed_point_tau_syn = float_to_fixed(self._tau_syn)
+        # fixed_point_tau_syn = float_to_fixed(self._tau_syn)
 
         spec.write_value(data=fixed_point_tau_f, data_type=DataType.INT32)
         spec.write_value(data=fixed_point_tau_d, data_type=DataType.INT32)
-        spec.write_value(data=fixed_point_tau_syn, data_type=DataType.INT32)
+        # spec.write_value(data=fixed_point_tau_syn, data_type=DataType.INT32)
 
         # Write lookup tables
         spec.write_array(self._tau_f_data)
         spec.write_array(self._tau_d_data)
-        spec.write_array(self._tau_syn_data)
+        # spec.write_array(self._tau_syn_data)
 
     @overrides(AbstractTimingDependence.get_parameter_names)
     def get_parameter_names(self):
-        return ['tau_f', 'tau_d', 'tau_syn']
+        return ['tau_f', 'tau_d']#, 'tau_syn']
 
     @property
     def synaptic_structure(self):
